3650f5603daee6cdea6b9f5640725f20361bed3b
Ajay Kumar <41580007+ajayks13@users.noreply.github.com>
2019-01-05 21:05:20 +0530

    Add files via upload

---
+++ AuctionSite (Bidding)/script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("enter your first name");
+var lastName = prompt("enter your last name");
+var age = prompt("enter your age");
+console.log("user's full name is " + firstName +" "+ lastName + ".");
+console.log("User's age is " + age );
